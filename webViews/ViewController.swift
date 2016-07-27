6fa7b49a0572907302e47461cd4c173e8863e25a
kaylant <kaylant@gmail.com>
2016-07-26 19:28:16 -0500

    working with two types of web views:

--- webViews/ViewController.swift
+++ webViews/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // set url of web view
+//        var url = NSURL(string: "https://www.google.com")
+//        
+//        var request = NSURLRequest(URL: url!)
+//        
+//        webView.loadRequest(request)
+        
+        // second option is to create the html ourselves
+        var html = "<html><body><h1>My Page</h1><p>This is my web page.</p></body></html>"
+        
+        // baseUrl would be useful for embedded links within the page
+        webView.loadHTMLString(html, baseURL: nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
